0da8660fbe83b5ca8ecefdcbfb9983e999ac7116
yazin24 <yazinconiendo@gmail.com>
2023-08-30 13:55:03 +0800

    ..

--- resources/views/admin/admin_sales_monitoring.blade.php
+++ resources/views/admin/admin_sales_monitoring.blade.php
@@ -4,13 +4,16 @@
     <div class="w-full">
         <h2 class="font-bold md:text-xl mb-2 ml-1">Sales Graph Monitoring</h2>
 
-        <div class="flex flex-col lg:flex-row gap-2" style="position: relative; height:38vh; width:80vw;">
-            <canvas id="shopeeChart" class="bg-gray-200" ></canvas>
-            <canvas id="lazadaChart" class="bg-gray-200"</canvas>
+        <div class="flex flex-col lg:flex-row gap-2 mb-2" style="height:38vh; width:80vw;">
+           
+                <canvas id="shopeeChart" class="bg-gray-200" ></canvas>
+                <canvas id="lazadaChart" class="bg-gray-200"</canvas>
+                  
         </div>
-        <div class="flex flex-col lg:flex-row gap-2 mt-2" style="position: relative; height:38vh; width:80vw;">
+
+        <div class="flex flex-col lg:flex-row gap-2" style="height:38vh; width:80vw;">
             <canvas id="manualChart" class="bg-gray-200" ></canvas>
-            {{-- <canvas id="lazadaChart" class="bg-gray-200"</canvas> --}}
+        <canvas id="donutChart" class="bg-gray-200"</canvas>
         </div>
     </div>
 
@@ -95,5 +98,30 @@
         });
     });
 
+    document.addEventListener('DOMContentLoaded', function () {
+        var ctx = document.getElementById('donutChart').getContext('2d');
+        
+        var userChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: @json($manualDates),
+                datasets: [{
+                    label: 'Manual Total Sales',
+                    data: @json($manualAmounts),
+                    backgroundColor: 'rgb(151,0,255,1)',
+                    borderColor: 'rgb(151,201,255,1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    });
+    
     </script>
 @endsection
