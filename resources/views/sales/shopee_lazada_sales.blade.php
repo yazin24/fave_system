6a9b4d6f8e651e814367f861228aeb0b49df6658
yazin24 <yazinconiendo@gmail.com>
2023-09-14 16:01:21 +0800

    ....

--- resources/views/sales/shopee_lazada_sales.blade.php
+++ resources/views/sales/shopee_lazada_sales.blade.php
@@ -2,7 +2,7 @@
 
 @section('sales-body')
         
-<div class="bg-white-900 text-gray-900 mt-2">
+<div class="bg-white-900 text-gray-900 mt-2 font-bold">
         <div class="flex flex-row justify-between">
                 <div>
                         <h2 class="font-bold text-orange-600">Shopee Orders</h2>
@@ -15,11 +15,13 @@
         <table class="bg-gray-300 shadow-lg w-full">
             <thead>
                 <tr class="bg-gray-900 border-b-2 text-gray-300 w-96 h-6">
-                    <th class="text-xs text-center w-1/5">ACTION</th>
+                    
                     <th class="text-xs text-center w-1/5">ORDER ID</th>
                     <th class="text-xs text-center w-1/5">FULL NAME</th>
+                    <th class="text-xs text-center w-1/5">ADDRESS</th>
                     <th class="text-xs text-center w-1/5">NUMBER</th>
-                    <th class="text-xs text-center w-2/5">ADDRESS</th>
+                    <th class="text-xs text-center w-1/5">ACTION</th>
+                    
                     
                     
                 </tr>
@@ -27,11 +29,12 @@
             <tbody>
                @foreach($allShopeeSales as $shopeeSale)
                 <tr class="h-6">
-                    <td class="border-b-2 text-xs text-center hover:underline text-red-600 hover:font-bold"><a href="{{route('shopeeorderdetails', ['shopeeSale' => $shopeeSale -> id])}}">View</a></td>
+                    
                     <td class="border-b-2 text-xs text-center">{{$shopeeSale -> order_id}}</td>
                     <td class="border-b-2 text-xs text-center">{{$shopeeSale -> customers_name}}</td>
+                    <td class="border-b-2 text-xs text-center text-[9px]">{{$shopeeSale -> customers_address}}</td>
                     <td class="border-b-2 text-xs text-center">{{$shopeeSale -> phone_number}}</td>
-                    <td class="border-b-2 text-xs text-center mx-auto max-w-md overflow-hidden whitespace-nowrap overflow-ellipsis">{{$shopeeSale -> customers_address}}</td>
+                    <td class="border-b-2 text-xs text-center hover:underline text-red-600 hover:font-bold"><a href="{{route('shopeeorderdetails', ['shopeeSale' => $shopeeSale -> id])}}">View</a></td>
                 </tr>
                @endforeach  
             </tbody>
@@ -54,24 +57,24 @@
         <table class="bg-gray-300 shadow-lg w-full">
             <thead>
                 <tr class="bg-gray-900 border-b-2 text-gray-300 w-96 h-6">
-                    <th class="text-xs text-center w-1/5">ACTION</th>
+                    
                         <th class="text-xs text-center w-1/5">ORDER NUMBER</th>
                         <th class="text-xs text-center w-1/5">FULL NAME</th>
-                        <th class="text-xs text-center w-1/5">NUMBER</th>
                         <th class="text-xs text-center w-1/5">ADDRESS</th>
+                        <th class="text-xs text-center w-1/5">NUMBER</th>
+                        <th class="text-xs text-center w-1/5">ACTION</th>
                         
                 </tr>
             </thead>
             <tbody>
                @foreach($allLazadaSales as $lazadaSale)
                 <tr class="h-6">
-                    <td class="border-b-2 text-xs text-center hover:underline text-red-600 hover:font-bold"><a href="{{route('lazadaorderdetails', ['lazadaSale' => $lazadaSale])}}">View</a></td>
+                    
                         <td class="border-b-2 text-xs text-center">{{$lazadaSale -> order_number}}</td>
                         <td class="border-b-2 text-xs text-center">{{$lazadaSale -> customers_name}}</td>
-                        
+                        <td class="border-b-2 text-xs text-center text-[9px]">{{$lazadaSale -> customers_address}}</td>
                         <td class="border-b-2 text-xs text-center">{{$lazadaSale -> phone_number}}</td>
-                        <td class="border-b-2 text-xs text-center mx-auto max-w-md overflow-hidden whitespace-nowrap overflow-ellipsis">{{$lazadaSale -> customers_address}}</td>
-                        
+                        <td class="border-b-2 text-xs text-center hover:underline text-red-600 hover:font-bold"><a href="{{route('lazadaorderdetails', ['lazadaSale' => $lazadaSale])}}">View</a></td>
                 </tr>
                @endforeach  
             </tbody>
