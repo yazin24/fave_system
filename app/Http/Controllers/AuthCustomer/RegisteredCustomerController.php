2653e4714a4855e29dffed1397dab32024dccc5a
yazin24 <yazinconiendo@gmail.com>
2023-09-20 11:27:06 +0800

    ....

--- app/Http/Controllers/AuthCustomer/RegisteredCustomerController.php
+++ app/Http/Controllers/AuthCustomer/RegisteredCustomerController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customers;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
 
 class RegisteredCustomerController extends Controller
 {
@@ -13,11 +15,27 @@ class RegisteredCustomerController extends Controller
         $request -> validate([
 
             'full_name' => 'required',
-            'email' => 'required',
+            'email' => ['required', 'email', 'unique:'.Customers::class],
             'phone' => 'required',
-            'password' => 'required',
-            
+            'password' =>['required', 'confirmed', Rules\Password::defaults()]
+
+        ], [
+            'full_name.required' => 'Full name is required',
+            'email.required' => 'Email is Required',
+            'phone.required' => 'Phone number is required',
+            'password.required' => 'Password is required',
+            'password.confirm' => 'Password do not match'
+        ]);
+
+        $customer = Customers::create([
+
+            'full_name' => $request -> full_name,
+            'email' => $request -> email,
+            'phone' => $request -> phone,
+            'password' => $request -> password,
 
         ]);
+
+        return view('ecommerce.login') -> with('success', 'Registered Successfully! You can now login.');
     }
 }
