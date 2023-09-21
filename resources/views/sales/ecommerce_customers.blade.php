5029c7ebb176678dafab719b945fbcd525fb1797
yazin24 <yazinconiendo@gmail.com>
2023-09-21 10:56:47 +0800

    ....

--- resources/views/sales/ecommerce_customers.blade.php
+++ resources/views/sales/ecommerce_customers.blade.php
@@ -2,11 +2,11 @@
 
 @section('sales-body')
 
-<h2 class="font-bold md:text-xl text-blue-900">Customers List And Details</h2>
+<h2 class="font-bold md:text-xl mt-2">Customers List</h2>
 
 
 <div class="flex flex-row justify-end">
-    <div class="mr-2">
+    {{-- <div class="mr-2">
         <button class="bg-gray-800 text-gray-200 p-1 rounded-sm text-sm font-bold">Customers</button>
     </div>
     <div class="mr-2">
@@ -20,18 +20,18 @@
     </div>
     <div class="">
         <button class="bg-gray-800 text-gray-200 p-1 rounded-sm text-sm font-bold">Sales</button>
-    </div>
+    </div> --}}
     
 </div>
 <div class="bg-white-900 text-gray-900 mt-1"> 
     <table class="w-full shadow-md bg-gray-400">
         <thead>
             <tr class="bg-gray-800 text-gray-200 h-8 md:h-10 text-xs">
-                <th class="text-xs w-1/6 font-bold">ORDER ID</th>
+                {{-- <th class="text-xs w-1/6 font-bold">ORDER ID</th> --}}
                 <th class="text-xs w-1/6 font-bold">CUSTOMERS NAME</th>
                 <th class="text-xs w-1/6 font-bold">PHONE NUMBER</th>
-                <th class="text-xs w-1/6 font-bold">STATUS</th>
-                <th class="text-xs w-1/6 font-bold">TOTAL AMOUNT</th>
+                <th class="text-xs w-1/6 font-bold">ADDRESS</th>
+                {{-- <th class="text-xs w-1/6 font-bold">TOTAL AMOUNT</th> --}}
                 <th class="text-xs w-1/6 font-bold">ACTION</th>
                 
             </tr>
@@ -40,8 +40,8 @@
         <tbody class="bg-gray-300">
             
             <tr class="h-10">   
-                <td class="text-xs text-center border-b-2 font-bold"></td>
-                <td class="text-xs text-center border-b-2 font-bold"></td>
+                {{-- <td class="text-xs text-center border-b-2 font-bold"></td>
+                <td class="text-xs text-center border-b-2 font-bold"></td> --}}
                 <td class="text-xs text-center border-b-2 font-bold">
                    
                 </td>
