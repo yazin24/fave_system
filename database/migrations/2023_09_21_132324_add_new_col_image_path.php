6b25bb58448db22fb8b6343f139d63b7691ca774
yazin24 <yazinconiendo@gmail.com>
2023-09-21 13:38:22 +0800

    .....

---
+++ database/migrations/2023_09_21_132324_add_new_col_image_path.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('product_sku', function (Blueprint $table) {
+            $table -> string('image_path') -> after('wholesale_price') -> default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('product_sku', function (Blueprint $table) {
+            //
+        });
+    }
+};
