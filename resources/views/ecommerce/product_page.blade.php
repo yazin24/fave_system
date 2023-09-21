ac667307a977b9fd28fc7f217dcf3ed649daf129
yazin24 <yazinconiendo@gmail.com>
2023-09-21 15:16:06 +0800

    ....

--- resources/views/ecommerce/product_page.blade.php
+++ resources/views/ecommerce/product_page.blade.php
@@ -9,9 +9,17 @@
 <div class="flex justify-center  rounded-sm w-72 mb-8 p-4 bg-gray-100 shadow-lg hover:scale-[1.05] hover: !scale-100!important duration-100">
   <div  class="w-72">
     <img src="{{asset($product -> image_path)}}">
-    <h2 class="mt-8">₱{{$product -> retail_price}}</h2>
-    <button class="bg-violet-700 hover:bg-violet-800 text-gray-200 p-1 rounded-sm font-bold w-full mb-1"><a><i class="fa-solid fa-cart-shopping"></i> Add To Cart</a></button>
-    <button class="bg-violet-700 hover:bg-violet-800 text-gray-200 p-1 rounded-sm font-bold w-full"><a>Buy Now</a></button>
+    <h2 class="mt-8 font-bold text-violet-700">₱{{$product -> retail_price}}</h2>
+    @if(auth('customers') -> check())
+
+    <button class="bg-violet-700 hover:bg-violet-800 text-gray-200 p-1 rounded-sm font-bold w-full mb-1"><a href="{{route('homepage')}}"><i class="fa-solid fa-cart-shopping"></i> Add To Cart</a></button>
+
+    <button class="bg-violet-700 hover:bg-violet-800 text-gray-200 p-1 rounded-sm font-bold w-full"><a href="{{route('homepage')}}">Buy Now</a></button>
+    @else
+    <button class="bg-violet-700 hover:bg-violet-800 text-gray-200 p-1 rounded-sm font-bold w-full mb-1"><a href="{{route('loginpage')}}"><i class="fa-solid fa-cart-shopping"></i> Add To Cart</a></button>
+
+    <button class="bg-violet-700 hover:bg-violet-800 text-gray-200 p-1 rounded-sm font-bold w-full"><a href="{{route('loginpage')}}">Buy Now</a></button>
+    @endif
   </div>
 </div>
 @endforeach
