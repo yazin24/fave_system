9f6909dfa26f14fb14bd9ec2d7a3344e0944d0df
yazin24 <yazinconiendo@gmail.com>
2023-09-22 17:56:18 +0800

    .....

--- app/Http/Controllers/EcommerceFunctionsController.php
+++ app/Http/Controllers/EcommerceFunctionsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\EcomCustomerCart;
-use App\Models\EcomCustomerOrderItems;
 use App\Models\EcomCustomerOrders;
 use App\Models\EcomCustomers;
 use App\Models\ProductSku;
@@ -109,7 +108,7 @@ class EcommerceFunctionsController extends Controller
         ]);
 
         foreach($productIds as $productId){
-            dd($productIds);
+            
             if(in_array($productId, $productOrders)){
                 $quantityProduct = $productOrderQuantity[$productId] ?? null;
                 $priceProduct = $productOrderPrice[$productId] ?? null;
@@ -117,16 +116,14 @@ class EcommerceFunctionsController extends Controller
                 if($productId && $quantityProduct && $priceProduct){
                     // $totaOrderAmount = $quantityProduct * $priceProduct;
 
-                    $newCustomerOrderItems  = new EcomCustomerOrderItems([
+                    $newCustomerOrder  -> ecomCustomerOrderItems() -> create([
 
-                        // 'order_id' => $newCustomerOrder -> id,
+                        'order_id' => $newCustomerOrder -> id,
                         'sku_id' => $productId,
                         'quantity' => $quantityProduct,
                         'price' => $priceProduct,
 
                     ]);
-
-                    $newCustomerOrder -> ecomCustomerOrderItems() -> save($newCustomerOrderItems);
                 }
             }
         }
