ecae6e96555f4a27311d10d2434d14afdcf581ef
yazin24 <yazinconiendo@gmail.com>
2023-09-23 11:48:11 +0800

    ....

--- resources/views/ecommerce/order_details_to_confirm.blade.php
+++ resources/views/ecommerce/order_details_to_confirm.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
 
-<h2>Order Details</h2>
+<div class="flex justify-center mt-8 font-bold">
+    <div class="bg-gray-100 w-3/4 md:w-1/2 rounded-sm shadow-md p-2">
+        <h2 class="mb-2">Order Details</h2>
+        <hr>
+
+        <div class="bg-white mt-1">
+            <img src="sdsd" alt="Image">
+        </div>
+        
+
+
+        <div class="flex justify-between mx-2 mt-4">
+            <button class="bg-red-500 hover:bg-red-600 p-1 text-sm text-gray-100 rounded-sm"><i class="fa-regular fa-rectangle-xmark mr-1"></i>Cancel</button>
+            <button class="bg-teal-500 hover:bg-teal-600 p-1 text-sm text-gray-100 rounded-sm"><i class="fa-regular fa-square-check fa-bounce mr-1"></i>Confirm Order</button>
+        </div>
+        
+    </div
+</div>
+
+
 
 @endsection
