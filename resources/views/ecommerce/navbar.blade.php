d60410a0a8eca447273f6efad75a11d8924ba294
yazin24 <yazinconiendo@gmail.com>
2023-09-28 09:46:16 +0800

    .....

--- resources/views/ecommerce/navbar.blade.php
+++ resources/views/ecommerce/navbar.blade.php
@@ -164,11 +164,14 @@
           </div>
         </nav>
 
+          <body>
+            <main>
+              <div>
+                @yield('content')
+               </div>
+            </main>
+          </body>
         
-
-        <div>
-         @yield('content')
-        </div>
 
         <footer>  
         <svg class="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220"><path fill="rgb(153, 51, 255)" fill-opacity="1" d="M0,160L80,154.7C160,149,320,139,480,144C640,149,800,171,960,154.7C1120,139,1280,85,1360,58.7L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
