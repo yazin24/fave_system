716e11005f4e94bde6201962f64a078e77199812
yazin24 <yazinconiendo@gmail.com>
2023-09-28 10:26:05 +0800

    ....

--- resources/views/ecommerce/login.blade.php
+++ resources/views/ecommerce/login.blade.php
@@ -1,19 +1,48 @@
 @extends('ecommerce.navbar')
 
 @section('content')
-
  
 <div class="font-bold text-red-500 font-2xl">
     @if (session('success'))
         {{ session('success')}}
     @endif
 </div>
-<div class="container flex justify-center items-center h-screen">
-    <div class="form-container p-4 bg-gradient-to-l from-violet-900 via-violet-400 to-blue-100 shadow backdrop-blur-sm drop-shadow-lg shadow-md h-3/4">
-        <div class="flex justify-center">
-        <img src="../images/newlogo.png" alt="" class="w-20 h-18 mt-20">
+
+<div class="mt-32 flex justify-center font-bold">
+
+    <div class="flex flex-col justify-center bg-violet-700 p-10 shadow-md rounded-sm">
+        <div class="flex justify-center mb-8">
+            <img src="../images/newlogo.png" alt="" class="w-20 h-18">
         </div>
-        <form method="POST" action="{{route('logincustomer')}}" class="w-full block mb-10 p-8 border border-solid border-gray-800 rounded-md flex flex-col space-y-2"> 
+        <form method="POST" action="{{route('logincustomer')}}"  class="flex flex-col justify-center"> 
+            @csrf
+        <input type="email" name="email" placeholder="Email" class="rounded-sm p-2 w-72 mb-2">
+
+        <input type="password" name="password" placeholder="Password" class="rounded-sm p-2 w-72">
+
+        <button type="submit" class="w-72 p-2 flex justify-center bg-stone-600 rounded-md text-white cursor mt-2 hover:bg-amber-500">Login</button>
+
+        <p class="mt-8 text-gray-100">Don't have an account?</p> 
+
+        <p class="underline cursor-pointer text-gray-100 hover:text-amber-300"><a href="{{route('registerpage')}}">Sign Up</a></p>
+        </form> 
+    </div>
+
+</div>
+
+@endsection
+
+
+
+{{-- <div class="flex flex-col justify-center items-center">
+
+    <div>
+        <img src="../images/newlogo.png" alt="" class="w-20 h-18 mt-20">
+    </div>
+    
+    <div class="items-center">
+
+        <form method="POST" action="{{route('logincustomer')}}"> 
             @csrf
             <input type="email" name="email" placeholder="Email" class="rounded-md p-2 w-72">
             <input type="password" name="password" placeholder="Password" class="rounded-md p-2 w-72">
@@ -23,7 +52,7 @@
             <p class="underline cursor-pointer"><a href="register">Sign Up</a></p>
             </div>
         </form>  
-    </div>
-</div>
 
-@endsection
+    </div>
+
+</div> --}}
--- resources/views/ecommerce/navbar.blade.php
+++ resources/views/ecommerce/navbar.blade.php
@@ -171,7 +171,7 @@
             </main>
         
         <footer class="mt-auto">  
-        <svg class="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220"><path fill="rgb(153, 51, 255)" fill-opacity="1" d="M0,160L80,154.7C160,149,320,139,480,144C640,149,800,171,960,154.7C1120,139,1280,85,1360,58.7L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+        <svg class="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220"><path fill="rgb(109 40 217)" fill-opacity="1" d="M0,160L80,154.7C160,149,320,139,480,144C640,149,800,171,960,154.7C1120,139,1280,85,1360,58.7L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
         </svg>
         <div class="px-16 lg:px-10 sm:px-0 md:pb-6 bord ">
           <div class="flex md:space-x-14 md:flex-row justify-between mx-8 grid grid-cols-1 md:grid-cols-3">
