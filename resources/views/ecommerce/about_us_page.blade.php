9e8606da7b78398d6323fe09312f8a86b93a71b5
yazin24 <yazinconiendo@gmail.com>
2023-09-28 11:03:01 +0800

    ....

--- resources/views/ecommerce/about_us_page.blade.php
+++ resources/views/ecommerce/about_us_page.blade.php
@@ -10,18 +10,24 @@
     <div class="flex flex-col md:flex-row md:mx-44 animate__animated animate__bounceInDown">
       <div class="p-8 md:p-12">
         <h2 class="text-2xl text-violet-700 font-bold mb-2">Mission</h2>
-        <p class="font-bold text-gray-800">
-          TO PROVIDE HIGH QUALITY PRODUCTS AND UNCONDITIONAL CUSTOMER
-          SATISFACTION THAT PROMOTES A HEALTHIER, SAFER, CLEANER ENVIRONMENT
-          AND MEETING CUSTOMER DEMANDS AND EXPECTATIONS DRIVEN BY CONTINOUS
-          RESEARCH AND PRODUCT INNOVATIONS. WE ARE COMMITED TO DELIVERING
-          ECO-FRIENDLY AND EFFECTIVE SOLUTIONS WHILE MINIMIZING THE IMPACT
-          ON THE PLANET DEVELOPMENT.
-        </p>
+        <div>
+
+        </div>
+        <div class="p-2 md:p-12 flex justify-center">
+          <i class="fa-solid fa-comment-dots text-8xl text-violet-700 md:mt-60 lg:mt-8"></i>
+        </div>
+        <div>
+          <p class="font-bold text-gray-800">
+            TO PROVIDE HIGH QUALITY PRODUCTS AND UNCONDITIONAL CUSTOMER
+            SATISFACTION THAT PROMOTES A HEALTHIER, SAFER, CLEANER ENVIRONMENT
+            AND MEETING CUSTOMER DEMANDS AND EXPECTATIONS DRIVEN BY CONTINOUS
+            RESEARCH AND PRODUCT INNOVATIONS. WE ARE COMMITED TO DELIVERING
+            ECO-FRIENDLY AND EFFECTIVE SOLUTIONS WHILE MINIMIZING THE IMPACT
+            ON THE PLANET DEVELOPMENT.
+          </p>
+        </div>
       </div>
-      <div class="p-2 md:p-12 flex justify-center">
-        <i class="fa-solid fa-comment-dots text-8xl text-violet-700 md:mt-60 lg:mt-8"></i>
-      </div>
+      
     </div>
 
 
