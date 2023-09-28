438fa460902508eaae3e18d1458cca515088b1c5
yazin24 <yazinconiendo@gmail.com>
2023-09-28 10:45:58 +0800

    ....

--- resources/views/ecommerce/navbar.blade.php
+++ resources/views/ecommerce/navbar.blade.php
@@ -40,6 +40,11 @@
                   ABOUT US
                   </a>
                 </li>
+                <li class="hover:bg-amber-500 py-6 px-6">
+                  <a href="{{route('aboutuspage')}}">
+                  ABOUT US
+                  </a>
+                </li>
                 @guest('customers')
                   <li class='mx-4 my-6 md:my-0'>
                     <a href="{{ route('loginpage') }}" class='text-yellow-600 md:text-gray-200 font-bold hover:bg-yellow-500 md:p-6 md:pt-10 duration-200'>LOGIN</a>
@@ -111,51 +116,55 @@
                         ABOUT US
                         </a>
                       </li>
-                      @guest('customers')
-<li class='mx-4 my-6 md:my-0'>
-    <a href="{{ route('loginpage') }}" class='text-yellow-600 md:text-gray-200 font-bold hover:bg-yellow-500 md:p-6 md:pt-10 duration-200'>LOGIN</a>
-</li>
-@else
-
-<li class='mx-4 my-6 md:my-0'>
-
-    <div class="hidden sm:flex sm:items-center sm:ml-6">
-        <x-dropdown align="right" width="48">
-            <x-slot name="trigger">
-                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                    {{-- <div class="capitalize text-xs">{{ Auth::customers()->name }}</div> --}}
-
-                    <div class="ml-1">
-                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                        </svg>
-                    </div>
-                </button>
-            </x-slot>
-
-            <x-slot name="content">
-                <x-dropdown-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-dropdown-link>
-
-                <x-dropdown-link :href="route('shoppingcart')">
-                 {{ __('Shopping Cart') }}
-             </x-dropdown-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logoutcustomer') }}">
-                    @csrf
-
-                    <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-dropdown-link>
-                </form>
-            </x-slot>
-        </x-dropdown>
-    </div>
-   
-</li>
-@endguest
+                      <li class="hover:bg-amber-500 py-4 px-6 w-full"">
+                        @guest('customers')
+                        <li class='mx-4 my-6 md:my-0'>
+                            <a href="{{ route('loginpage') }}" class='text-yellow-600 md:text-gray-200 font-bold hover:bg-yellow-500 md:p-6 md:pt-10 duration-200'>LOGIN</a>
+                        </li>
+                        @else
+                        
+                        <li class='mx-4 my-6 md:my-0'>
+                        
+                            <div class="hidden sm:flex sm:items-center sm:ml-6">
+                                <x-dropdown align="right" width="48">
+                                    <x-slot name="trigger">
+                                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                            {{-- <div class="capitalize text-xs">{{ Auth::customers()->name }}</div> --}}
+                        
+                                            <div class="ml-1">
+                                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                </svg>
+                                            </div>
+                                        </button>
+                                    </x-slot>
+                        
+                                    <x-slot name="content">
+                                        <x-dropdown-link :href="route('profile.edit')">
+                                            {{ __('Profile') }}
+                                        </x-dropdown-link>
+                        
+                                        <x-dropdown-link :href="route('shoppingcart')">
+                                         {{ __('Shopping Cart') }}
+                                     </x-dropdown-link>
+                        
+                                        <!-- Authentication -->
+                                        <form method="POST" action="{{ route('logoutcustomer') }}">
+                                            @csrf
+                        
+                                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+                                                {{ __('Log Out') }}
+                                            </x-dropdown-link>
+                                        </form>
+                                    </x-slot>
+                                </x-dropdown>
+                            </div>
+                           
+                        </li>
+                        @endguest
+                        
+                      </li>
+                    
                     </ul>
                   </div>
               </button>
@@ -223,7 +232,4 @@
 
      </body>
 </html>
-
-
-
 
