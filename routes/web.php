4cdcd92e8f424935e62c3993955ff91b5fb7ce4a
yazin24 <yazinconiendo@gmail.com>
2023-10-03 11:38:23 +0800

    ....

--- routes/web.php
+++ routes/web.php
@@ -81,9 +81,9 @@ Route::get('inventory-history', [InventoryController::class, 'inventory_history'
 //----------------------------------------------------------ECOMMERCE-----------------------------------------------------------------
 //----------------------------------------------------------ECOMMERCE-----------------------------------------------------------------
 
-    Route::group(['domain' => 'fave.com.ph'], function(){
+    // Route::group(['domain' => 'fave.com.ph'], function(){
 
-        Route::get('/fave/home', [EcommerceController::class, 'home_page']) -> name('homepage');
+    Route::get('/fave/home', [EcommerceController::class, 'home_page']) -> name('homepage');
 
     Route::get('/fave/products', [EcommerceController::class, 'product_page']) -> name('productpage');
     
@@ -137,7 +137,7 @@ Route::get('inventory-history', [InventoryController::class, 'inventory_history'
     });
 
 
-    });
+    // });
 
 
 
