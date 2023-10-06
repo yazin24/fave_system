2eb8bae28319349e652ee38fc1e16a91b147a9c1
yazin24 <yazinconiendo@gmail.com>
2023-10-06 09:36:19 +0800

    ....

---
+++ database/migrations/2023_10_06_093335_add_del_charge_and_total_amount.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('purchase_orders', function (Blueprint $table) {
+            
+            $table -> decimal('del_charge', 8, 2) -> nullable() -> after('approve_by');
+
+            $table -> decimal('total_amount', 8, 2) -> nullable() -> after('del_charge');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('purchase_orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
