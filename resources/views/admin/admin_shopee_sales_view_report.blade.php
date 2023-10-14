cc5e5aad73a20933c5de8ecef1cd0c9b14cfc449
yazin24 <yazinconiendo@gmail.com>
2023-10-14 10:18:19 +0800

    ....

--- resources/views/admin/admin_shopee_sales_view_report.blade.php
+++ resources/views/admin/admin_shopee_sales_view_report.blade.php
@@ -2,16 +2,20 @@
 
 @section('admin-body')
 
+
+<form>
     <div class="flex flex-row items-center gap-1">
+       
         <select class="text-xs rounded-sm">
-            <option>-----------</option>
+            <option>----------------------</option>
             <option>Daily</option>
             <optiion>Weekly</optiion>
             <option>Monthly</option>
             <option>Quarterly</option>
             <option>Yearly</option>
         </select>
-        <button class="bg-teal-500 hover:bg-teal-600 font-bold p-1.5 text-gray-200 rounded-md"><i class="fa-solid fa-right-to-bracket"></i></button>
+        <button type="submit" class="bg-teal-500 hover:bg-teal-600 font-bold p-1.5 text-gray-200 rounded-md"><i class="fa-solid fa-right-to-bracket"></i> Generate</button>
     </div>
+</form>
 
 @endsection
